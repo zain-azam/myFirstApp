7d87450be1dc6c8b4e6ce58c9e83134e6592488d
zain-azam <99675234+zain-azam@users.noreply.github.com>
2022-02-14 21:20:29 +0500

    Add files via upload

---
+++ basic-server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer(function( req, res){
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(200); //status code HTTP 200 / OK
+
+    let dataObj = { id: 123, name: "zainazam", email: "zmmirza3@gmail.com"};
+    let data = JSON.stringify(dataObj);
+    res.end(data);
+});
+
+server.listen(1234, function(){
+    console.log("Listening on port 1234");
+});
